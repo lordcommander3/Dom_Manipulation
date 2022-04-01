1511a12a7c998e430e491257e072c36e8cdf986b
zaryab <zaryabsheikh6@gmail.com>
2022-04-01 19:32:15 +0500

    changing pictures with function and onclick etc

---
+++ app.js
@@ -0,0 +1,22 @@
+
+function openText(){
+    var text="Lorem ipsum dolor, sit amet consectetur adipisicing elit. Magnam cupiditate placeat necessitatibus voluptatibus odit, minus vero distinctio consequuntur odio inventore officiis labore quo hic ipsam repudiandae libero tempora cumque cum!"
+    var paragraph=document.getElementById('para1')
+    paragraph.innerHTML=text
+} //application of html in JavaScript
+
+function openList(){
+    var text="<ul><li>List 1</li></ul><ul><li>List 2</li></ul><ul><li>List 3</li></ul><ul><li>List 4</li></ul>"
+    var paragraph=document.getElementById('para2')
+    paragraph.innerHTML=text
+} //application of html in JavaScript
+
+function cancer(){
+    var img2=document.getElementById('img2')
+    img2.src="https://iowalum.com/wp-content/uploads/2021/10/128694-cancer.webp"
+}
+function cigarette(){
+    var img2=document.getElementById('img2')
+    img2.src="https://images.theconversation.com/files/318202/original/file-20200303-18266-1944cnt.jpg?ixlib=rb-1.1.0&q=45&auto=format&w=754&fit=clip"
+
+}
